1d1e94b6669fdea2ce1df00ee9b36383525ca1f4
Abhimanyu Deora <abhikdeora@gmail.com>
2015-03-02 18:50:22 -0500

    added generating the backwards markov model

--- Markov2.java
+++ Markov2.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
@@ -10,6 +8,7 @@ import java.util.Random;
  */
 public class Markov2 {
     public static Hashtable<String, ArrayList> forwardMarkov = new Hashtable();
+    public static Hashtable<String, ArrayList> backwardMarkov = new Hashtable();
     Random rnd = new Random();
 
     public Hashtable trainForward( File file )
@@ -98,24 +97,106 @@ public class Markov2 {
                 }
             }
         }
-        catch(Exception ex) {ex.printStackTrace();}
+        catch(Exception ex) {
+            System.out.println("");
+            ex.printStackTrace();
+        }
         return forwardMarkov;
     }
 
-    public static void main(String args[])
+    public Hashtable trainBackward( File file )
     {
-        Markov2 m = new Markov2();
-        Hashtable forward = m.trainForward(new File("prince.txt"));
-        System.out.println(forward);
-        m.generateForward("Israel");
-        System.out.println("");
-        m.generateForward("Machiavelli");
-        System.out.println("");
-        m.generateForward("A");
-        System.out.println("");
+        ArrayList<ArrayList<String>> endArrayList = new ArrayList<ArrayList<String>>();
+        endArrayList.add(new ArrayList<String>());
+        endArrayList.add(new ArrayList<String>());
+        endArrayList.add(new ArrayList<String>());
+        backwardMarkov.put("_end", endArrayList);
+        backwardMarkov.put("_start", new ArrayList<ArrayList<String>>());
+
+        String thisLine;
+
+        try
+        {
+            BufferedReader br = new BufferedReader( new FileReader( file ) );
+            while ( ( thisLine = br.readLine() ) != null ) {
+                String[] wordList = thisLine.split( " " );
+                for ( int i = 0; i < wordList.length; i++ ) {
+                    //if starting words, no words before it
+                    if ( i == 0 )
+                    {
+                        ArrayList<String> startWords = backwardMarkov.get( "_start" );
+                        startWords.add( wordList[i] );
+                    }
+                    // if last word, add words before it
+                    else if ( i == wordList.length - 1 )
+                    {
+                        // get the arraylist of end words
+                        ArrayList<ArrayList<String>> endWords = backwardMarkov.get( "_end" );
+
+                        //add the first end word
+                        endWords.get( 0 ).add( wordList[i] );
+
+                        if ( wordList.length > 1 ) {
+                            // add the next word to the list of start words
+                            endWords.get( 1 ).add( wordList[i - 1] );
+                            if ( wordList.length > 2 ) {
+                                // add the third word to the list of start words
+                                endWords.get( 2 ).add( wordList[i - 2] );
+                            }
+                        }
+                    }
+                    //if any word in the middle
+                    else
+                    {
+                        ArrayList<ArrayList<String>> followingWords = backwardMarkov.get( wordList[i] );
+
+                        //get the current and three following words
+                        String currentWord = wordList[i];
+                        String firstPreviousWord = wordList[i - 1];
+                        String secondPreviousWord = "";
+                        String thirdPreviousWord = "";
+                        if (i > 1)
+                        {
+                            secondPreviousWord = wordList[i - 2];
+                            if (i > 2)
+                            {
+                                thirdPreviousWord = wordList[i - 3];
+                            }
+                        }
+
+                        // if the current word hasn't been added yet
+                        if (followingWords == null)
+                        {
+                            ArrayList<String> firstWords = new ArrayList<String>();
+                            ArrayList<String> secondWords = new ArrayList<String>();
+                            ArrayList<String> thirdWords = new ArrayList<String>();
+                            firstWords.add(firstPreviousWord);
+                            secondWords.add(secondPreviousWord);
+                            thirdWords.add(thirdPreviousWord);
+                            ArrayList<ArrayList<String>> wordsToAdd = new ArrayList<ArrayList<String>>();
+                            wordsToAdd.add(firstWords);
+                            wordsToAdd.add(secondWords);
+                            wordsToAdd.add(thirdWords);
+                            backwardMarkov.put(currentWord, wordsToAdd);
+                        }
+                        else
+                        {
+                            followingWords.get( 0 ).add(firstPreviousWord);
+                            followingWords.get ( 1 ).add(secondPreviousWord);
+                            followingWords.get( 2 ).add(thirdPreviousWord);
+                        }
+                    }
+                }
+            }
+        }
+        catch(Exception ex) {
+            System.out.println("");
+            ex.printStackTrace();
+        }
+        return backwardMarkov;
     }
 
-    public void generateForward(String keyword)
+    public String generateForward(String keyword)
     {
         /*  1) start with keyword
         *   2) choose random first following word
@@ -124,39 +205,67 @@ public class Markov2 {
          *  5) repeat using third following word as keyword
          *  6) Stop when last character of any word has a period
         */
-        System.out.print(keyword + " ");
         String firstWord;
         String secondWord;
         String thirdWord;
-        while ( keyword.charAt(keyword.length() - 1) != '.' )
+        String output = keyword + " ";
+        while (keyword.charAt(keyword.length() - 1) != '.')
         {
             firstWord = secondWord = thirdWord = "";
             ArrayList<ArrayList<String>> wordsList = forwardMarkov.get(keyword);
+            if ( wordsList == null )
+            {
+                System.out.println("ERROR! WORD NOT FOUND!");
+                break;
+            }
             ArrayList<String> firstWords = wordsList.get( 0 );
             ArrayList<String> secondWords = wordsList.get( 1 );
             ArrayList<String> thirdWords = wordsList.get( 2 );
             firstWord = firstWords.get( new Random().nextInt(firstWords.size()) );
             if (firstWord.charAt(firstWord.length() - 1) == '.')
             {
-                System.out.print(firstWord);
+                output += firstWord + " ";
                 break;
             }
             secondWord = secondWords.get( new Random().nextInt(secondWords.size()) );
-            if (secondWord.charAt(secondWord.length() - 1) == '.')
-            {
-                System.out.print(secondWord);
+            if (secondWord.charAt(secondWord.length() - 1) == '.') {
+                output += secondWord + " ";
                 break;
             }
             thirdWord = thirdWords.get( new Random().nextInt(thirdWords.size()));
-            if (secondWord.charAt(secondWord.length() - 1) == '.')
-            {
-                System.out.print(thirdWord);
+            if (thirdWord.charAt(thirdWord.length() - 1) == '.') {
+                output += thirdWord + " ";
                 break;
             }
-            System.out.print(firstWord + " " + secondWord + " " + thirdWord + " ");
+            output += firstWord + " " + secondWord + " " + thirdWord + " ";
             keyword = thirdWord;
         }
-
+        return output;
     }
+
+    public String generateBackward(String keyword)
+    {
+        
+    }
+
+    public static void main(String args[])
+    {
+        Markov2 m = new Markov2();
+        File trainingData = new File("prince.txt");
+        /*Hashtable backward = m.trainBackward(trainingData);
+        File file = new File("output.txt");
+        try {
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(backward.toString());
+            bw.close();
+            System.out.println("File written");
+        }
+        catch(IOException ex){System.out.println("IOException");}*/
+        m.trainForward(trainingData);
+        System.out.println(m.generateForward("the"));
+    }
+
+
 
 }
